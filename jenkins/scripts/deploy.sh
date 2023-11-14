b52b4cbc7fd0c759a3e9ea4c3b43d90bafbe6b9d
afifazfar07 <afif.azfar@gmail.com>
2023-11-14 12:10:40 +0800

    update

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app --network jenkins -v "C:/Users/afifa/Desktop/SIT Modules/ICT3203/Labs/jenkins-php-selenium-test:/var/www/html" php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
