eef8d37b9bd13569ed6adaaaeb0250b5e51d9b77
afifazfar07 <afif.azfar@gmail.com>
2023-11-14 12:39:53 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app --network jenkins -v "C:/Users/afifa/Desktop/SIT Modules/ICT3203/Labs/jenkins-php-selenium-test:/var/www/html" php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v "C:/Users/afifa/Desktop/SIT Modules/ICT3203/Labs/jenkins-php-selenium-test:/var/www/html" php:7.2-apache
 sleep 1
 set +x
 
